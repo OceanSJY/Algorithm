52dca8faa307decf4916978d0dc01b0161b12030
Ocean Sun <jiayang.sun@yungoal.com>
2018-06-21 22:46:46 +0800

    Implement the bubble sort in 3 ways

---
+++ src/AlgorithmByCSharp/BubbleSort/BubbleSortHelper.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    /// <summary>
+    /// The bubble sort helper
+    /// </summary>
+    public class BubbleSortHelper
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="intCollection"></param>
+        /// <returns></returns>
+        public static IEnumerable<int> DynamicBubbleSort(IEnumerable<int> intCollection)
+        {
+            var intArray = intCollection.ToArray();
+
+            for (var i = 0; i < intArray.Length; i++)
+            {
+                for (var j = 0; j < intArray.Length - 1 - i; j++)
+                {
+                    if (intArray[j] <= intArray[j + 1])
+                    {
+                        continue;
+                    }
+
+                    var temp = intArray[j];
+                    intArray[j] = intArray[j + 1];
+                    intArray[j + 1] = temp;
+                }
+            }
+
+            return intArray;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="intCollection"></param>
+        /// <returns></returns>
+        public static IEnumerable<int> FixedBubbleSort(IEnumerable<int> intCollection)
+        {
+            var intArray = intCollection.ToArray();
+
+            for (var i = 0; i < intArray.Length; i++)
+            {
+                for (var j = 0; j < intArray.Length; j++)
+                {
+                    if (intArray[i] >= intArray[j])
+                    {
+                        continue;
+                    }
+
+                    var temp = intArray[i];
+                    intArray[i] = intArray[j];
+                    intArray[j] = temp;
+                }
+            }
+
+            return intArray;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="intCollection"></param>
+        /// <returns></returns>
+        public static IEnumerable<int> BubbleSortByWhile(IEnumerable<int> intCollection)
+        {
+            var intArray = intCollection.ToArray();
+            var swapped = true;
+
+            do
+            {
+                swapped = false;
+                for (var i = 0; i < intArray.Length - 1; i++)
+                {
+                    if (intArray[i] <= intArray[i + 1])
+                    {
+                        continue;
+                    }
+
+                    var temp = intArray[i];
+                    intArray[i] = intArray[i + 1];
+                    intArray[i + 1] = temp;
+
+                    swapped = true;
+                }
+            } while (swapped);
+
+            return intArray;
+        }
+    }
+}
---
+++ src/AlgorithmByCSharp/BubbleSort/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var intCollection = new List<int>()
+            {
+                1,
+                3,
+                22,
+                44,
+                32,
+                5,
+                7,
+                0,
+                11,
+                23,
+                2,
+                2,
+                1,
+                4,
+                56,
+                2,
+                3,
+                6,
+                7,
+                4,
+                8,
+                3,
+                31,
+                5,
+                51,
+                34,
+                4,
+                345,
+                1,
+                23
+            };
+
+            var resultFromDynamic = BubbleSortHelper.DynamicBubbleSort(intCollection);
+            var resultFromFixed = BubbleSortHelper.FixedBubbleSort(intCollection);
+            var resultFromWhile = BubbleSortHelper.BubbleSortByWhile(intCollection);
+
+            Console.ReadLine();
+        }
+    }
+}
